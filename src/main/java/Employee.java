1f3e92d842bdbe860e8cc4040422e04cdb48526a
unknown <raje1reddy@gmail.com>
2019-08-14 20:34:37 +0100

    my initial code for overloaded constructors

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -2,10 +2,28 @@
 class Employee {
     String name;
     int age;
-    Employee(){
-        age = 20;
-        System.out.println("user defined constructor");
 
+    //No argument constructor
+    Employee() {
+        name = "John";
+        age = 25;
+    }
+
+    //Constructor with one argument (String)
+    Employee(String newName) {
+        name = newName;
+        //age = 25;
+    }
+
+    //Constructor with two arguments(int,String)
+    Employee(int newAge, String newName) {
+        name = newName;
+        age = newAge;
+    }
+
+    //Constructor with two arguments(String,int)
+    Employee(String newName, int newAge) {
+        name = newName;
+        age = newAge;
     }
 }
-
